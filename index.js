0e398b8af054bca3c587f8a3426ad28655cccb4c
HuntsberryS <112701256+HuntsberryS@users.noreply.github.com>
2024-02-19 22:05:57 -0500

    search working

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-function searchWeather(event) {
-  event.preventDefault();
-  let input = document.querySelector("#searchCity");
-  let cityElement = document.querySelector(".city");
-  cityElement.innerHTML = input.value;
+function updateWeatherInfo(response) {
+  console.log(response.data.temperature.current);
+  let currentTemp = document.querySelector(".weatherNumericalValue");
+  let tempDisplay = Math.round(response.data.temperature.current);
+
+  let cityElement = document.querySelector(".displayCity");
+  cityElement.innerHTML = response.data.city;
+
+  currentTemp.innerHTML = tempDisplay;
 }
 
-let form = document.querySelector("#search-form");
-form.addEventListener("submit", searchWeather);
+function searchCity(city) {
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=7036a98t226cf2o3c044afd3b96a58b7&units=imperial`;
+  let apiKey = "7036a98t226cf2o3c044afd3b96a58b7";
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
+function displayInputCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#searchCity");
+  searchCity(searchInput.value);
+}
+
+let searchFormElement = document.querySelector("#searchForm");
+searchFormElement.addEventListener("submit", displayInputCity);
